6777c9b2b782c0d16774f81eaec5f022bf4e4ad8
VijayaRaghavaPonnaganti <vponnaga@kent.edu>
2024-09-21 21:00:11 +0000

    Initial commit for Flask Pet App

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    species = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Pet {self.name}>'
+
+@app.route('/')
+def index():
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pet():
+    if request.method == 'POST':
+        name = request.form['name']
+        species = request.form['species']
+        new_pet = Pet(name=name, species=species)
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_pet.html')
+
+@app.route('/delete/<int:pet_id>')
+def delete_pet(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    db.session.delete(pet)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create the database and tables
+    app.run(debug=True)
