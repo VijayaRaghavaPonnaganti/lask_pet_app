3c21379748ea797758948b1c526e1cd127431586
VijayaRaghavaPonnaganti <vponnaga@kent.edu>
2024-10-23 21:54:08 -0400

    Update pets.py

--- pets.py
+++ pets.py
@@ -1,1 +1,11 @@
+from app import db1, Pet 
+#inserting some pets
+petA = Pet(name="A",species="RAT", age=1)
+petB = Pet(name="B",species="PANDA",age=7)
+petC = Pet(name="C",species="CAT",age=4)
 
+db1.session.add(petA)
+db1.session.add(petB)
+db1.session.add(petC)
+
+db.session.commit()
